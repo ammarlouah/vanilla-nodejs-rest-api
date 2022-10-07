9ebf67c3d45fb5a0a25011b5d6da7f3884f661e2
Ammar <ammarlouah9@gmail.com>
2022-10-07 15:46:57 +0100

    HTTP create server and send a simple response

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+require('dotenv').config();
+const products = require('./data/products.json');
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.end(JSON.stringify(products));
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,()=>console.log(`Server running on PORT ${PORT}....`));
